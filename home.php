cfb3a32135cb9edbfbbd97f78dc9d1f04c0e5cc4
Maria Mayer <mayermaru@gmail.com>
2017-04-27 12:04:35 -0300

    Cambiar links en home y README

--- home.php
+++ home.php
@@ -6,25 +6,25 @@
 get_header(); ?>
 
 <div class="container-fluid">
-	<section class="intro content-section">
+	<section id="intro" class="content-section">
 		<img src="<?php echo get_template_directory_uri(); ?>/img/intro.png">
 	</section>
-	<section class="servicios content-section">
+	<section id="servicio" class="content-section">
 		<img src="<?php echo get_template_directory_uri(); ?>/img/servicios.png">
 	</section>
-	<section class="tecnologias content-section">
+	<section id="tecnologias" class="content-section">
 		<img src="<?php echo get_template_directory_uri(); ?>/img/tecnologias.png">
 	</section>
-	<section class="proyectos content-section">
+	<section id="proyectos" class="content-section">
 		<img src="<?php echo get_template_directory_uri(); ?>/img/proyectos.png">
 	</section>
-	<section class="productos content-section">
+	<section id="productos" class="content-section">
 		<img src="<?php echo get_template_directory_uri(); ?>/img/productos.png">
 	</section>
-	<section class="blog content-section">
+	<section id="blog content-section">
 		<img src="<?php echo get_template_directory_uri(); ?>/img/blog.png">
 	</section>
-	<section class="contactos content-section">
+	<section id="contactos" class="content-section">
 		<img src="<?php echo get_template_directory_uri(); ?>/img/contactos.png">
 	</section>
 
